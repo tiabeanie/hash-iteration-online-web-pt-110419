bbdb51696e46d1425c7316c8d4b196a213fa2dd6
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-14 20:34:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,8 +12,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if birthday_kids age <= 12 
+    if age <= 12 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
     end
   end
 end 
